4cc0365c2ac832658ea894d357a2297a6667e234
Iyana Medlock <iyanamedlock@gmail.com>
2020-12-24 23:35:53 -0600

    coded a lil

--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,9 @@ var now = moment().format('dddd, MMMM Do');
 const CURRENTDAY = $('#currentDay')
 CURRENTDAY.text(now);
 
-workday = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM']
-
+let workday = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM']
+let todoList = [];
+let index = [];
 
 
 function render() {
@@ -23,6 +24,7 @@ function render() {
        label.attr('id','time');
        row.append(label);
        textarea.attr('id', 'textarea');
+       textarea.attr('data-index', i);
        textarea.attr('class', 'past');
        row.append(textarea);
        row.append(button);
@@ -35,7 +37,19 @@ render();
 
 $('button').on('click', function(){
     if ($(this).hasClass('saveBtn')){
-       
+      let textVal = $(this).parent('.row').find('#textarea');
+      let indexVal = textVal.attr('data-index');
+        if (textVal.val().trim().length === 0){
+            alert('invalid input');
+            textVal.val('');
+            return;
+        }
+        else{
+            todoList.push(textVal.val());
+            index.push(indexVal);
+            console.log(todoList);
+            console.log(index);
+        }
         
     }
     else{
